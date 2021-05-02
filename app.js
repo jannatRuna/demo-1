350f15410f05dc41865b1b2e90b5c17a8a0ff198
Runa Akter <75990022+jannatRuna@users.noreply.github.com>
2021-05-02 14:56:47 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+function creatBubble(){
+    const section = document.querySelector('.bubble')
+    const createElement = document.createElement('span')
+    var size = Math.random() * 60;
+
+    createElement.style.width = 20+ size + 'px';
+    createElement.style.height = 20 + size + 'px';
+    createElement.style.left = Math.random() * innerWidth + "px";
+    section.appendChild(createElement);
+
+    setTimeout(() => {
+        createElement.remove()
+    },4000)
+}
+setInterval(creatBubble,50);
